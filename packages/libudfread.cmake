1f12b4e423c06b14e06eda48d3db460ff3ee0d1f
Kobayashi <koba@somedco.eu.org>
2025-08-14 06:09:38 +0700

    mujs: migrate to new repo

--- packages/libudfread.cmake
+++ packages/libudfread.cmake
@@ -5,10 +5,10 @@ ExternalProject_Add(libudfread
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} CONF=1 meson setup <BINARY_DIR> <SOURCE_DIR>
         --prefix=${MINGW_INSTALL_PREFIX}
-		--libdir=${MINGW_INSTALL_PREFIX}/lib
-    	--cross-file=${MESON_CROSS}
-		--buildtype=release
-    	--default-library=static
+        --libdir=${MINGW_INSTALL_PREFIX}/lib
+        --cross-file=${MESON_CROSS}
+        --buildtype=release
+        --default-library=static
     BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
     INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
